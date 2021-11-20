999e48514a3d2e219ed31a07757d2e11216de75a
ahsouza <annibalhsouza@gmail.com>
2021-11-20 11:33:15 -0300

    refactor: optimizing text message

--- cli/cli.go
+++ cli/cli.go
@@ -15,18 +15,18 @@ func GenerateWin() {
 
 	app := &cli.App{
 		Name:        "<seu-projeto>",
-		Usage:       "Nomear o nome da pasta de origem para o diretório do projeto",
+		Usage:       "Crie uma nova pasta de origem para o diretório do projeto",
 		Description: "Barca CLI é uma ferramenta para gerar projetos em diversas tecnologias trazendo maior produtividade no desenvolvimento.",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:        "lang, l",
-				Value:       "português-brasileiro",
+				Value:       "portugues-brasileiro",
 				Usage:       "Idioma da exibição",
 				Destination: &language,
 			},
 			&cli.StringFlag{
 				Name:  "config, c",
-				Usage: "Carregar configurações através de um arquivo `JSON/TOML` ",
+				Usage: "Carregar configurações através de arquivo `JSON/TOML` ",
 			},
 		},
 		Commands: []*cli.Command{
@@ -34,8 +34,8 @@ func GenerateWin() {
 			{
 				Name:        "init",
 				Aliases:     []string{"i"},
-				Usage:       "Criar projeto",
-				Description: "Inicie a tecnologia de seu projeto",
+				Usage:       "Criar um projeto de software",
+				Description: "Inicie a tecnologia que você deseja",
 				Subcommands: []*cli.Command{
 					//API REST *********************  API REST   ***************************** API REST  ***********************
 					{
@@ -45,9 +45,7 @@ func GenerateWin() {
 						Description: "Implementar Web Server para servir endpoints",
 						Action: func(c *cli.Context) error {
 							directory := c.Args().First()
-
 							generate.API(language, directory, nameProject)
-
 							return nil
 						},
 					},
