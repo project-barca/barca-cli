b71bf2cce6eb90920441fcdc3bda82177126ab1a
ahsouza <annibalhsouza@gmail.com>
2021-11-21 21:22:46 -0300

    feat: implement feature to generate web express server and install json package dependencies

--- generate/node/write/js/mainExpress.go
+++ generate/node/write/js/mainExpress.go
@@ -2,13 +2,53 @@ package write
 
 import (
 	dependencies "barca-cli/generate/node/dependencies"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"time"
+
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"github.com/pterm/pterm"
+	"golang.org/x/text/language"
 )
 
+var localizer *i18n.Localizer
+var bundle *i18n.Bundle
+
 func ServerExpress(port string, directory string, lang string) {
+	// Configurações para internacionalização do software
+	bundle = i18n.NewBundle(language.English)
+	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
+	bundle.LoadMessageFile("translate/en.json")
+	bundle.LoadMessageFile("translate/fr.json")
+	bundle.LoadMessageFile("translate/pt-BR.json")
+
+	switch lang {
+	case "portugues-brasileiro":
+		localizer = i18n.NewLocalizer(bundle, language.BrazilianPortuguese.String(), language.English.String(), language.French.String())
+	case "english":
+		localizer = i18n.NewLocalizer(bundle, language.English.String(), language.BrazilianPortuguese.String(), language.French.String())
+	case "francais":
+		localizer = i18n.NewLocalizer(bundle, language.French.String(), language.English.String(), language.BrazilianPortuguese.String())
+	default:
+		localizer = i18n.NewLocalizer(bundle, language.BrazilianPortuguese.String(), language.English.String(), language.French.String())
+	}
+
+	localizeSuccessExpressServer := i18n.LocalizeConfig{
+		MessageID: "success_server_express",
+	}
+	localizeSuccessNodeModules := i18n.LocalizeConfig{
+		MessageID: "success_node_modules",
+	}
+	localizeInstallModulesNpm := i18n.LocalizeConfig{
+		MessageID: "install_modules_npm",
+	}
+	resultInstallNpmModules, _ := localizer.Localize(&localizeInstallModulesNpm)
+	resultExpressServer, _ := localizer.Localize(&localizeSuccessExpressServer)
+	resultNodeModules, _ := localizer.Localize(&localizeSuccessNodeModules)
+
 	_, errorPath := os.Stat("./" + directory)
 
 	if os.IsNotExist(errorPath) {
@@ -45,15 +85,28 @@ func ServerExpress(port string, directory string, lang string) {
 		if err3 != nil {
 			log.Fatal(err3)
 		}
-		go dependencies.PackageJson(directory, "express.js", lang)
-
-		cmd := exec.Command("npm", "install", directory)
+		pterm.Success.Println(resultExpressServer)
+		dependencies.PackageJson(directory, "express.js", lang)
+		dependencies.AutoInstallNpm(directory)
+		//go file.Move(directory, ".bat")
 		curr_wd, err := os.Getwd()
+
+		spinnerSuccess, _ := pterm.DefaultSpinner.Start(resultInstallNpmModules)
+		time.Sleep(time.Second * 5)
+		spinnerSuccess.Success()
+		//cmd := exec.Command("cmd", "start", "/c", curr_wd+"\\install.bat")
+		cmd := exec.Command("cmd", "start", "/c", curr_wd+"\\installMod.bat")
+		if err := cmd.Start(); err != nil {
+			fmt.Println("Error: ", err)
+		}
+		//fmt.Println(curr_wd + "\\" + directory)
+		//curr_wd, err := os.Getwd()
 		//cmd := exec.Command("cd", curr_wd+"/hao", "&", "npm", "run", "install")
+		cmd.Wait()
 		stdout, err := cmd.Output()
-		fmt.Println("done")
-		fmt.Println(curr_wd)
-		fmt.Print(string(stdout))
+		pterm.Success.Println(resultNodeModules)
+		//fmt.Println(curr_wd)
+		fmt.Print(stdout)
 
 	}
 }
