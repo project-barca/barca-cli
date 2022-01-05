ce351218176a973b4cf9d8526c433b15e890e8b0
ahsouza <annibalhsouza@gmail.com>
2022-01-05 16:20:46 -0300

    fix: rename env variable barca

--- utils/dir/fileSystemScan.go
+++ utils/dir/fileSystemScan.go
@@ -11,7 +11,7 @@ func Scan(path string, f os.FileInfo, err error) error {
 	//fmt.Printf("\n\nArquivo Verificado:     %s\nTamanho do arquivo:     %d bytes\n", path, f.Size())
 	// string to write to file
 	bytes := []byte(path + "\n")
-	ENV_BARCA := os.Getenv("BARCA_TEMP_LIST_DIR")
+	ENV_BARCA := os.Getenv("BARCA_TMP_FILE")
 	// open file for writing
 	fd1, err := syscall.Open(ENV_BARCA, syscall.O_CREAT|syscall.O_RDWR|syscall.O_APPEND, 0777)
 	if err != nil {
