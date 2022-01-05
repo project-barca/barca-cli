8f3206a99148737bd87b414df08af574d87063e5
ahsouza <annibalhsouza@gmail.com>
2022-01-05 16:19:35 -0300

    feat: add methods to scanner path project

---
+++ utils/scan/scanDirectory.go
@@ -0,0 +1,28 @@
+package scan
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"barca-cli/utils/dir"
+	"barca-cli/utils/file"
+)
+
+func Directory(directory string) []string {
+	file.NewTempFile("barco-cli.tmp.txt", "BARCA_TMP_FILE")
+	dir.Scanner(directory)
+
+	input, err := ioutil.ReadFile(os.Getenv("BARCA_TMP_FILE"))
+	if err != nil {
+		log.Println(err)
+	}
+	lines := strings.Split(string(input), "\n")
+
+	return lines
+}
+
+func GetExtensions(directory string) []string {
+	return file.WhatsIsExtension(Directory(directory))
+}
