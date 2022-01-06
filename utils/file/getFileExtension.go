0643161cdbe973857a39a63f4df25219d0e7a479
ahsouza <annibalhsouza@gmail.com>
2022-01-06 09:54:14 -0300

    fix: remove repetitive elements in the extension chain from scanned files

--- utils/file/getFileExtension.go
+++ utils/file/getFileExtension.go
@@ -4,10 +4,22 @@ import (
 	"path/filepath"
 )
 
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
 func WhatsIsExtension(fileName []string) []string {
 	var extensions []string
+
 	for i := 0; i < len(fileName); i++ {
-		extensions = append(extensions, filepath.Ext(fileName[i]))
+		if contains(extensions, filepath.Ext(fileName[i])) != true {
+			extensions = append(extensions, filepath.Ext(fileName[i]))
+		}
 	}
 	return extensions
 }
