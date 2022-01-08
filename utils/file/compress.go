5c075cda4fdb19fd6e0b21008bda4c6feb53347f
ahsouza <annibalhsouza@gmail.com>
2022-01-08 13:08:58 -0300

    feat: add method to choice options in compression format

--- utils/file/compress.go
+++ utils/file/compress.go
@@ -1,39 +1,79 @@
 package file
 
 import (
+	"archive/zip"
 	"compress/gzip"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
-func Compress(inFile string) {
-	// Open file for reading
-	file, err := os.Open(inFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Read file content
-	data, err := ioutil.ReadAll(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Close file
-	file.Close()
-	// Create .gz file to write to
-	outputFile, err := os.Create(inFile + ".gz")
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Create a gzip writer on top of file writer
-	gzipWriter := gzip.NewWriter(outputFile)
-	// Write out compressed data
-	_, err = gzipWriter.Write(data)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Close writer
-	gzipWriter.Close()
+func Compress(inFile string, ext string) {
+	switch ext {
+	case "gz":
+		// Open file for reading
+		file, err := os.Open(inFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Read file content
+		data, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Close file
+		file.Close()
+		// Create .gz file to write to
+		outputFile, err := os.Create(inFile + ".gz")
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Create a gzip writer on top of file writer
+		gzipWriter := gzip.NewWriter(outputFile)
+		// Write out compressed data
+		_, err = gzipWriter.Write(data)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Close writer
+		gzipWriter.Close()
 
-	log.Println("Arquivo " + inFile + " compactado com sucesso!")
+		log.Println("Arquivo " + inFile + " compactado com sucesso!")
+	case "zip":
+		newfile, err := os.Create(inFile + ".zip")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer newfile.Close()
+
+		zipit := zip.NewWriter(newfile)
+
+		defer zipit.Close()
+
+		zipfile, err := os.Open(inFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer zipfile.Close()
+
+		// get the file information
+		info, err := zipfile.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			log.Fatal(err)
+		}
+		header.Method = zip.Deflate
+		writer, err := zipit.CreateHeader(header)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = io.Copy(writer, zipfile)
+		log.Println("Arquivo " + inFile + " compactado com sucesso!")
+	}
+
 }
