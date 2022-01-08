2825fd274c4c61ea663c04e6cdc0404c3e4509e2
ahsouza <annibalhsouza@gmail.com>
2022-01-08 13:25:16 -0300

    feat: implement method to remove large or small size files

---
+++ utils/file/deleteFile.go
@@ -0,0 +1,90 @@
+package file
+
+import (
+	"fmt"
+	"math"
+	"os"
+)
+
+func RemoveFile(fileName string) {
+	var targetFile = fileName
+
+	file, err := os.OpenFile(targetFile, os.O_RDWR, 0666)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer file.Close()
+	// find out how large is the target file
+	fileInfo, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+	// calculate the new slice size
+	// base on how large our target file is
+	var size int64 = fileInfo.Size()
+	zeroBytes := make([]byte, size)
+
+	// fill out the new slice with 0 value
+	copy(zeroBytes[:], "0")
+	fmt.Println(zeroBytes[:])
+
+	// wipe the content of the target file
+	n, err := file.Write([]byte(zeroBytes))
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Wiped %v bytes.\n", n)
+
+	// finally remove/delete our file
+	err = os.Remove(targetFile)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func RemoveBigFile(fileName string) {
+	var targetFile = fileName
+
+	file, err := os.OpenFile(targetFile, os.O_RDWR, 0666)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer file.Close()
+	// find out how large is the target file
+	fileInfo, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+	// calculate the new slice size
+	// base on how large our target file is
+	var fileSize int64 = fileInfo.Size()
+	const fileChunk = 1 * (1 << 20) // 1 MB, change this to your requirement
+
+	// calculate total number of parts the file will be chunked into
+	totalPartsNum := uint64(math.Ceil(float64(fileSize) / float64(fileChunk)))
+
+	lastPosition := 0
+
+	for i := uint64(0); i < totalPartsNum; i++ {
+		partSize := int(math.Min(fileChunk, float64(fileSize-int64(i*fileChunk))))
+		partZeroBytes := make([]byte, partSize)
+		// fill out the part with zero value
+		copy(partZeroBytes[:], "0")
+		// over write every byte in the chunk with 0
+		fmt.Println("Writing to target file from position : ", lastPosition)
+		n, err := file.WriteAt([]byte(partZeroBytes), int64(lastPosition))
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("Wiped %v bytes.\n", n)
+		// update last written position
+		lastPosition = lastPosition + partSize
+	}
+	// finally remove/delete our file
+	err = os.Remove(targetFile)
+	if err != nil {
+		panic(err)
+	}
+}
