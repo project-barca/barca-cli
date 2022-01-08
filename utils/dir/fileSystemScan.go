dbcd01935343aac143de0f8bcae6cade2fba822b
ahsouza <annibalhsouza@gmail.com>
2022-01-08 15:34:13 -0300

    feat: implement method to get disk usage in directory

--- utils/dir/fileSystemScan.go
+++ utils/dir/fileSystemScan.go
@@ -7,6 +7,37 @@ import (
 	"syscall"
 )
 
+func DiskUsage(path string, info os.FileInfo) int64 {
+	size := info.Size()
+
+	if !info.IsDir() {
+		return size
+	}
+
+	dir, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+		return size
+	}
+	defer dir.Close()
+
+	files, err := dir.Readdir(-1)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, file := range files {
+		if file.Name() == "." || file.Name() == ".." {
+			continue
+		}
+		size += DiskUsage(path+"/"+file.Name(), file)
+	}
+	fmt.Printf("Tamanho em bytes: [%d] : [%s]\n", size, path)
+
+	return size
+}
+
 func Scan(path string, f os.FileInfo, err error) error {
 	//fmt.Printf("\n\nArquivo Verificado:     %s\nTamanho do arquivo:     %d bytes\n", path, f.Size())
 	// string to write to file
