73cc2caeafaa2ed69dd6f13f5361cb5cd5b3ca3a
ahsouza <annibalhsouza@gmail.com>
2022-01-09 23:15:54 -0300

    feat: add method to load public key RSA

--- utils/scan/key.go
+++ utils/scan/key.go
@@ -2,13 +2,14 @@ package scan
 
 import (
 	"crypto/dsa"
+	"crypto/rsa"
 	"encoding/gob"
 	"fmt"
 	"os"
 )
 
-func KeyDSA(publicKey string) {
-	//load public key
+func pubKeyDSA(publicKey string) {
+	//Public key DSA
 	pubKeyFile, err := os.Open(publicKey)
 	if err != nil {
 		fmt.Println(err)
@@ -25,10 +26,32 @@ func KeyDSA(publicKey string) {
 	}
 
 	pubKeyFile.Close()
-	fmt.Printf("Public key : \n%x\n", pubKeyFile)
+	fmt.Printf("Public key: \n%x\n", pubKeyFile)
 
 	fmt.Printf("Public key parameter P: %v\n", publickey.Parameters.P)
 	fmt.Printf("Public key parameter Q: %v\n", publickey.Parameters.Q)
 	fmt.Printf("Public key parameter G: %v\n", publickey.Parameters.G)
 	fmt.Printf("Public key Y: %v\n", publickey.Y)
+}
+
+func pubKeyRSA(publicKey string) {
+	// Public Key RSA
+	publickeyfile, err := os.Open(publicKey)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	decoder := gob.NewDecoder(publickeyfile)
+
+	var publickey rsa.PublicKey
+	err = decoder.Decode(&publickey)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	publickeyfile.Close()
+	fmt.Printf("Public key: \n%x\n", publickey)
 }
