17ecd45b388590c36b883c30db96fdba88cf826c
ahsouza <annibalhsouza@gmail.com>
2022-01-09 23:50:43 -0300

    feat: add metho to generate key RSA

--- generate/key.go
+++ generate/key.go
@@ -4,6 +4,8 @@ import (
 	"crypto/dsa"
 	"crypto/md5"
 	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
 	"encoding/asn1"
 	"encoding/gob"
 	"encoding/pem"
@@ -110,4 +112,54 @@ func KeyDSA() {
 
 	verifystatus = dsa.Verify(&pubkey, signhash, r, s)
 	fmt.Println(verifystatus) // should be false
+}
+
+func KeyRSA() {
+	// generate private key
+	privatekey, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		fmt.Println(err.Error)
+		os.Exit(1)
+	}
+
+	var publickey *rsa.PublicKey
+	publickey = &privatekey.PublicKey
+	// save private and public key separately
+	privatekeyfile, err := os.Create("RSAprivate.key")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	privatekeyencoder := gob.NewEncoder(privatekeyfile)
+	privatekeyencoder.Encode(privatekey)
+	privatekeyfile.Close()
+
+	publickeyfile, err := os.Create("RSApublic.key")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	publickeyencoder := gob.NewEncoder(publickeyfile)
+	publickeyencoder.Encode(publickey)
+	publickeyfile.Close()
+
+	// save PEM file
+	pemfile, err := os.Create("RSAprivate.pem")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var pemkey = &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privatekey)}
+
+	err = pem.Encode(pemfile, pemkey)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	pemfile.Close()
 }
