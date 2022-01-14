0f1a12a433d307789d1e6537bc2d2b5a6f41e31b
ahsouza <annibalhsouza@gmail.com>
2022-01-14 11:05:08 -0300

    feat: add entity to motherboard

---
+++ internal/entities/baseboard.go
@@ -0,0 +1,62 @@
+package entities
+
+// **************************************************************************************
+// **************************       XML DATA      ***************************************
+// **************************************************************************************
+type DriversXml struct {
+	URL  string `xml:"url"`
+	Name string `xml:"name"`
+	Size string `xml:"size"`
+}
+type AboutBaseboardXml struct {
+	Name         string `xml:"name"`
+	Version      string `xml:"version"`
+	SerialNumber string `xml:"serial_number"`
+	Vendor       string `xml:"vendor"`
+	BIOSversion  string `xml:"bios_version"`
+}
+type BaseboardXml struct {
+	AboutBaseboardXml AboutBaseboardXml `xml:"about"`
+	DriversXml        []DriversXml      `xml:"drivers"`
+}
+
+// **************************************************************************************
+// **************************        MONGODB      ***************************************
+// **************************************************************************************
+type DriversMongo struct {
+	URL  string `json:"url,omitempty" bson:"url,omitempty"`
+	Name int    `json:"name,omitempty" bson:"name,omitempty"`
+	Size int    `json:"size,omitempty" bson:"size,omitempty"`
+}
+type AboutBaseboardMongo struct {
+	Name         string `json:"name,omitempty" bson:"name,omitempty"`
+	Version      string `json:"version,omitempty" bson:"version,omitempty"`
+	SerialNumber string `json:"serial_number,omitempty" bson:"serial_number,omitempty"`
+	Vendor       string `json:"vendor,omitempty" bson:"vendor,omitempty"`
+	BIOSversion  string `json:"bios_version,omitempty" bson:"bios_version,omitempty"`
+}
+
+type BaseboardMongo struct {
+	AboutBaseboardMongo AboutBaseboardMongo `json:"about,omitempty" bson:"about,omitempty"`
+	DriversMongo        []DriversMongo      `json:"drivers,omitempty" bson:"drivers,omitempty"`
+}
+
+// **************************************************************************************
+// **************************************************************************************
+type Drivers struct {
+	URL  string
+	Name int
+	Size int
+}
+type AboutBaseboard struct {
+	Name         string
+	Version      string
+	SerialNumber string
+	Vendor       string
+	BIOSversion  string
+}
+
+type Baseboard struct {
+	AboutBaseboard AboutBaseboard
+	Drivers        []Drivers
+}
