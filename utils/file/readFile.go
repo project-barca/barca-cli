7c63803a4bcfa656a621900a69f4805b055dfe8c
ahsouza <annibalhsouza@gmail.com>
2022-01-18 10:28:34 -0300

    feat: add method to read a file csv

--- utils/file/readFile.go
+++ utils/file/readFile.go
@@ -1,9 +1,12 @@
 package file
 
 import (
+	"bufio"
 	"encoding/json"
 	"encoding/xml"
+	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func ReadXML(fileName string, key interface{}) interface{} {
@@ -22,4 +25,22 @@ func ReadJSON(fileName string, key interface{}) interface{} {
 	_ = json.Unmarshal([]byte(data), &interfaceKey)
 
 	return interfaceKey
+}
+
+func ReadCSV(fileName string) []string {
+	file, _ := os.Open(fileName)
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []string
+
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+	file.Close()
+
+	for _, eachline := range txtlines {
+		fmt.Println(eachline)
+	}
+
+	return txtlines
 }
