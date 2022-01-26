6199d276ab897ae4674fc30cc38fdc0254ebadef
ahsouza <annibalhsouza@gmail.com>
2022-01-26 00:49:39 -0300

    work: add auto generate functions to c/c++

--- generate/node/make/add/functions.go
+++ generate/node/make/add/functions.go
@@ -1793,3 +1793,358 @@ func FunctionRust(lang, directory, collection, database, method string, hidden b
 	}
 
 }
+
+func FunctionCplus(lang, directory, collection, database, method string, hidden bool, inputs []string) {
+	bundle = i18n.NewBundle(language.English)
+	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
+	bundle.LoadMessageFile("translate/en.json")
+	bundle.LoadMessageFile("translate/fr.json")
+	bundle.LoadMessageFile("translate/pt-BR.json")
+
+	switch lang {
+	case "portugues-brasileiro":
+		localizer = i18n.NewLocalizer(bundle, language.BrazilianPortuguese.String(), language.English.String(), language.French.String())
+	case "english":
+		localizer = i18n.NewLocalizer(bundle, language.English.String(), language.BrazilianPortuguese.String(), language.French.String())
+	case "francais":
+		localizer = i18n.NewLocalizer(bundle, language.French.String(), language.English.String(), language.BrazilianPortuguese.String())
+	default:
+		localizer = i18n.NewLocalizer(bundle, language.BrazilianPortuguese.String(), language.English.String(), language.French.String())
+	}
+
+	_, errorPath := os.Stat("./" + directory)
+
+	if hidden != true {
+
+		switch method {
+		case "create":
+			localizeConfigErrorModelFunctionsPHP := i18n.LocalizeConfig{
+				MessageID: "error_functions_php",
+			}
+			localizeConfigSuccessFunctionsPHP := i18n.LocalizeConfig{
+				MessageID: "success_functions_php",
+			}
+			resultErrorModelFunctionsPHP, _ := localizer.Localize(&localizeConfigErrorModelFunctionsPHP)
+			resultSuccessFunctionsPHP, _ := localizer.Localize(&localizeConfigSuccessFunctionsPHP)
+
+			if os.IsNotExist(errorPath) {
+				pterm.Error.Println(resultErrorModelFunctionsPHP)
+			} else {
+				path := directory + "/c"
+
+				if _, err := os.Stat(path); os.IsNotExist(err) {
+					os.Mkdir(path, 0755)
+				}
+
+				if file.CheckIfExists(path+"/"+collection+".c") != false {
+					indexLineFile := file.FindPositionLineText(path+"/"+collection+".c", "void insert"+collection+"(")
+					if indexLineFile == 0 {
+						functiondata := []string{"\nvoid insert" + collection + "() \n{"}
+						var strParamns string
+						if inputs != nil {
+							for _, input := range inputs {
+								strParamns += input + ", "
+								functiondata = append(functiondata, "    "+input+", ")
+							}
+						}
+						functiondata = append(functiondata, "}")
+
+						fileCurrent, err := os.OpenFile(path+"/"+collection+".c", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+						if err != nil {
+							log.Fatalf("Falha ao tentar modificar o arquivo: %s", err)
+						}
+
+						datawriter := bufio.NewWriter(fileCurrent)
+						for _, data := range functiondata {
+							_, _ = datawriter.WriteString(data + "\n")
+						}
+						datawriter.Flush()
+						fileCurrent.Close()
+
+						file.ReplaceString(path+"/"+collection+".c", "insert"+collection+"()", "insert"+collection+"("+strParamns+")")
+					} else {
+						fmt.Println("Já existe está função CREATE")
+					}
+				} else {
+					f, err := os.Create(path + "/" + collection + ".c")
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer f.Close()
+
+					firstLine := "\nvoid insert" + collection + "()\n{\n"
+					data := []byte(firstLine)
+
+					_, err2 := f.Write(data)
+					if err2 != nil {
+						log.Fatal(err2)
+					}
+					// Verify paramns input values - start
+					if inputs != nil {
+						var strParamns string
+						for _, input := range inputs {
+							strParamns += input + ", "
+							_, err3 := f.WriteString("    " + input + ";\n")
+							if err3 != nil {
+								log.Fatal(err3)
+							}
+						}
+						f.WriteString("\n")
+
+						errStr := file.InsertStringToFile(path+"/"+collection+".c", "}", file.GetNumberLines(path+"/"+collection+".c")-1)
+						if errStr != nil {
+							log.Fatal(errStr)
+						}
+
+						file.ReplaceString(path+"/"+collection+".c", "insert"+collection+"()", "insert"+collection+"("+strParamns+")")
+					}
+					// Verify paramns input values - end
+				}
+
+				pterm.Success.Println(resultSuccessFunctionsPHP)
+			}
+
+		case "read":
+			localizeConfigErrorModelFunctionsPHP := i18n.LocalizeConfig{
+				MessageID: "error_functions_php",
+			}
+			localizeConfigSuccessFunctionsPHP := i18n.LocalizeConfig{
+				MessageID: "success_functions_php",
+			}
+			resultErrorModelFunctionsPHP, _ := localizer.Localize(&localizeConfigErrorModelFunctionsPHP)
+			resultSuccessFunctionsPHP, _ := localizer.Localize(&localizeConfigSuccessFunctionsPHP)
+
+			if os.IsNotExist(errorPath) {
+				pterm.Error.Println(resultErrorModelFunctionsPHP)
+			} else {
+				path := directory + "/c"
+
+				if _, err := os.Stat(path); os.IsNotExist(err) {
+					os.Mkdir(path, 0755)
+				}
+
+				if file.CheckIfExists(path+"/"+collection+".c") != false {
+					indexLineFile := file.FindPositionLineText(path+"/"+collection+".c", "void read"+collection+"(")
+					if indexLineFile == 0 {
+						functiondata := []string{"\nvoid read" + collection + "()\n{"}
+						var strParamns string
+						if inputs != nil {
+							for _, input := range inputs {
+								strParamns += input + ", "
+								functiondata = append(functiondata, "    "+input+",")
+							}
+						}
+						functiondata = append(functiondata, "}")
+
+						fileCurrent, err := os.OpenFile(path+"/"+collection+".c", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+						if err != nil {
+							log.Fatalf("Falha ao tentar modificar o arquivo: %s", err)
+						}
+
+						datawriter := bufio.NewWriter(fileCurrent)
+						for _, data := range functiondata {
+							_, _ = datawriter.WriteString(data + "\n")
+						}
+						datawriter.Flush()
+						fileCurrent.Close()
+
+						file.ReplaceString(path+"/"+collection+".c", "read"+collection+"()", "read"+collection+"("+strParamns+")")
+					} else {
+						fmt.Println("Já existe está função READ")
+					}
+				} else {
+					f, err := os.Create(path + "/" + collection + ".c")
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer f.Close()
+
+					firstLine := "\nvoid read" + collection + "()\n{\n"
+					data := []byte(firstLine)
+
+					_, err2 := f.Write(data)
+					if err2 != nil {
+						log.Fatal(err2)
+					}
+					// Verify paramns input values - start
+					if inputs != nil {
+						var strParamns string
+						for _, input := range inputs {
+							strParamns += input + ", "
+							_, err3 := f.WriteString("    " + input + ",\n")
+							if err3 != nil {
+								log.Fatal(err3)
+							}
+						}
+						f.WriteString("\n")
+
+						errStr := file.InsertStringToFile(path+"/"+collection+".c", "}", file.GetNumberLines(path+"/"+collection+".c")-1)
+						if errStr != nil {
+							log.Fatal(errStr)
+						}
+
+						file.ReplaceString(path+"/"+collection+".c", "read"+collection+"()", "read"+collection+"("+strParamns+")")
+					}
+					// Verify paramns input values - end
+				}
+				pterm.Success.Println(resultSuccessFunctionsPHP)
+			}
+
+		case "delete":
+			localizeConfigErrorModelFunctionsPHP := i18n.LocalizeConfig{
+				MessageID: "error_functions_php",
+			}
+			localizeConfigSuccessFunctionsPHP := i18n.LocalizeConfig{
+				MessageID: "success_functions_php",
+			}
+			resultErrorModelFunctionsPHP, _ := localizer.Localize(&localizeConfigErrorModelFunctionsPHP)
+			resultSuccessFunctionsPHP, _ := localizer.Localize(&localizeConfigSuccessFunctionsPHP)
+
+			if os.IsNotExist(errorPath) {
+				pterm.Error.Println(resultErrorModelFunctionsPHP)
+			} else {
+				path := directory + "/c"
+
+				if _, err := os.Stat(path); os.IsNotExist(err) {
+					os.Mkdir(path, 0755)
+				}
+
+				if file.CheckIfExists(path+"/"+collection+".c") != false {
+					indexLineFile := file.FindPositionLineText(path+"/"+collection+".c", "void delete"+collection+"(")
+					if indexLineFile == 0 {
+						functiondata := []string{"\nvoid delete" + collection + "(int id)\n{",
+							"}",
+						}
+						file, err := os.OpenFile(path+"/"+collection+".c", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+						if err != nil {
+							log.Fatalf("Falha ao tentar modificar o arquivo: %s", err)
+						}
+
+						datawriter := bufio.NewWriter(file)
+						for _, data := range functiondata {
+							_, _ = datawriter.WriteString(data + "\n")
+						}
+						datawriter.Flush()
+						file.Close()
+					} else {
+						fmt.Println("Já existe está função DELETE")
+					}
+				} else {
+					f, err := os.Create(path + "/" + collection + ".c")
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer f.Close()
+
+					firstLine := "\nvoid delete" + collection + "(int id)\n{\n"
+					data := []byte(firstLine)
+
+					_, err2 := f.Write(data)
+					if err2 != nil {
+						log.Fatal(err2)
+					}
+
+					line2 := "id; \n}"
+					data2 := []byte(line2)
+
+					var idx int64 = int64(len(data))
+
+					_, err3 := f.WriteAt(data2, idx)
+					if err3 != nil {
+						log.Fatal(err3)
+					}
+				}
+				pterm.Success.Println(resultSuccessFunctionsPHP)
+			}
+		case "update":
+			localizeConfigErrorModelFunctionsJavacript := i18n.LocalizeConfig{
+				MessageID: "error_functions_javascript",
+			}
+			localizeConfigSuccessFunctionsJavascript := i18n.LocalizeConfig{
+				MessageID: "success_functions_javascript",
+			}
+			resultErrorModelFunctionsJavascript, _ := localizer.Localize(&localizeConfigErrorModelFunctionsJavacript)
+			resultSuccessFunctionsJavascript, _ := localizer.Localize(&localizeConfigSuccessFunctionsJavascript)
+
+			if os.IsNotExist(errorPath) {
+				pterm.Error.Println(resultErrorModelFunctionsJavascript)
+			} else {
+				path := directory + "/c"
+
+				if _, err := os.Stat(path); os.IsNotExist(err) {
+					os.Mkdir(path, 0755)
+				}
+
+				if file.CheckIfExists(path+"/"+collection+".c") != false {
+					indexLineFile := file.FindPositionLineText(path+"/"+collection+".c", "function update"+collection+"(")
+					if indexLineFile == 0 {
+						functiondata := []string{"\nvoid update" + collection + "()\n{"}
+						var strParamns string
+						if inputs != nil {
+							for _, input := range inputs {
+								strParamns += input + ", "
+								functiondata = append(functiondata, "    "+input+",")
+							}
+						}
+						functiondata = append(functiondata, "}")
+
+						fileCurrent, err := os.OpenFile(path+"/"+collection+".c", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+						if err != nil {
+							log.Fatalf("Falha ao tentar modificar o arquivo: %s", err)
+						}
+
+						datawriter := bufio.NewWriter(fileCurrent)
+						for _, data := range functiondata {
+							_, _ = datawriter.WriteString(data + "\n")
+						}
+						datawriter.Flush()
+						fileCurrent.Close()
+
+						file.ReplaceString(path+"/"+collection+".c", "update"+collection+"()", "update"+collection+"("+strParamns+")")
+					} else {
+						fmt.Println("Já existe está função UPDATE")
+					}
+				} else {
+					f, err := os.Create(path + "/" + collection + ".c")
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer f.Close()
+
+					firstLine := "void update" + collection + "()\n{\n"
+					data := []byte(firstLine)
+
+					_, err2 := f.Write(data)
+					if err2 != nil {
+						log.Fatal(err2)
+					}
+					// Verify paramns input values - start
+					if inputs != nil {
+						var strParamns string
+						for _, input := range inputs {
+							strParamns += input + ", "
+							_, err3 := f.WriteString("    " + input + ",\n")
+							if err3 != nil {
+								log.Fatal(err3)
+							}
+						}
+						f.WriteString("\n")
+
+						errStr := file.InsertStringToFile(path+"/"+collection+".c", "}", file.GetNumberLines(path+"/"+collection+".c")-1)
+						if errStr != nil {
+							log.Fatal(errStr)
+						}
+
+						file.ReplaceString(path+"/"+collection+".c", "update"+collection+"()", "update"+collection+"("+strParamns+")")
+					}
+					// Verify paramns input values - end
+				}
+
+				pterm.Success.Println(resultSuccessFunctionsJavascript)
+			}
+		default:
+			// barcaSQL
+		}
+	}
+
+}
