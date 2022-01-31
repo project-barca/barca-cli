7cc239bdf2f8230edad91bdad629cc2870214c57
ahsouza <annibalhsouza@gmail.com>
2022-01-31 10:22:23 -0300

    feat: add func to create scripts

--- utils/file/createFile.go
+++ utils/file/createFile.go
@@ -1,14 +1,34 @@
 package file
 
 import (
-	"fmt"
+	"log"
 	"os"
 )
 
 func New(fileName string, destination string) {
 	newFile, err := os.Create(destination + "/" + fileName)
 	if err != nil {
-		fmt.Println("Error:", err)
+		log.Fatal(err)
 	}
 	newFile.Close()
+}
+
+func NewScript(cmd, fileName, destination string) string {
+
+	f, err := os.Create(destination + "/" + fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	line := cmd
+	data := []byte(line)
+
+	_, err2 := f.Write(data)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	return "Script criado: " + fileName
 }
