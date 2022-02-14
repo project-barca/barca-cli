f739208f3072dba28cf2ab70d643a31fd850de25
ahsouza <annibalhsouza@gmail.com>
2022-02-14 18:53:50 -0300

    feat: add new methods to generate api using bottle python framework

---
+++ generate/py/bottle/serve.go
@@ -0,0 +1,108 @@
+package bottle
+
+import (
+	dependencies "barca-cli/generate/node/dependencies"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"github.com/pterm/pterm"
+	"golang.org/x/text/language"
+)
+
+var localizer *i18n.Localizer
+var bundle *i18n.Bundle
+
+func Server(port, directory, lang string) {
+	// Configurações para internacionalização do software
+	bundle = i18n.NewBundle(language.English)
+	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
+	bundle.LoadMessageFile("translate/en.json")
+	bundle.LoadMessageFile("translate/fr.json")
+	bundle.LoadMessageFile("translate/pt-BR.json")
+
+	switch lang {
+	case "portugues-brasileiro":
+		localizer = i18n.NewLocalizer(bundle, language.BrazilianPortuguese.String(), language.English.String(), language.French.String())
+	case "english":
+		localizer = i18n.NewLocalizer(bundle, language.English.String(), language.BrazilianPortuguese.String(), language.French.String())
+	case "francais":
+		localizer = i18n.NewLocalizer(bundle, language.French.String(), language.English.String(), language.BrazilianPortuguese.String())
+	default:
+		localizer = i18n.NewLocalizer(bundle, language.BrazilianPortuguese.String(), language.English.String(), language.French.String())
+	}
+
+	localizeSuccessExpressServer := i18n.LocalizeConfig{
+		MessageID: "success_server_express",
+	}
+	localizeSuccessNodeModules := i18n.LocalizeConfig{
+		MessageID: "success_node_modules",
+	}
+	localizeInstallModulesNpm := i18n.LocalizeConfig{
+		MessageID: "install_modules_npm",
+	}
+	resultInstallNpmModules, _ := localizer.Localize(&localizeInstallModulesNpm)
+	resultExpressServer, _ := localizer.Localize(&localizeSuccessExpressServer)
+	resultNodeModules, _ := localizer.Localize(&localizeSuccessNodeModules)
+
+	_, errorPath := os.Stat("./" + directory)
+
+	if os.IsNotExist(errorPath) {
+		errDir := os.Mkdir("./"+directory, 0777)
+		if errDir != nil {
+			log.Fatal(errorPath)
+			return
+		}
+
+		f, err := os.Create("./" + directory + "/main.py")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer f.Close()
+
+		line := "from bottle import Bottle, run\n"
+		data := []byte(line)
+
+		_, err2 := f.Write(data)
+
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+
+		line2 := "\napp = Bottle()\n\n@app.route('/')\ndef home():\n    return \"Olá, você está usando Barca CLI\"\n\nrun(app, host='localhost', port=" + port + ")\n"
+		data2 := []byte(line2)
+
+		var idx int64 = int64(len(data))
+
+		_, err3 := f.WriteAt(data2, idx)
+
+		if err3 != nil {
+			log.Fatal(err3)
+		}
+		pterm.Success.Println(resultExpressServer)
+		dependencies.PackagePip(directory, "main.py", lang, "bottle")
+		dependencies.AutoInstallPip(directory)
+		//go file.Move(directory, ".bat")
+		curr_wd, err := os.Getwd()
+
+		spinnerSuccess, _ := pterm.DefaultSpinner.Start(resultInstallNpmModules)
+		time.Sleep(time.Second * 5)
+		spinnerSuccess.Success()
+		//cmd := exec.Command("cmd", "start", "/c", curr_wd+"\\install.bat")
+		cmd := exec.Command("cmd", "start", "/c", curr_wd+"\\installMod.bat")
+		if err := cmd.Start(); err != nil {
+			fmt.Println("Error: ", err)
+		}
+		//fmt.Println(curr_wd + "\\" + directory)
+		//curr_wd, err := os.Getwd()
+		//cmd := exec.Command("cd", curr_wd+"/hao", "&", "npm", "run", "install")
+		cmd.Wait()
+		pterm.Success.Println(resultNodeModules)
+	}
+}
