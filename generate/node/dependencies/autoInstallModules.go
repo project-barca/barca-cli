12b4037fb200d2c86cbfe229dad3e850f7aa9b61
ahsouza <annibalhsouza@gmail.com>
2022-02-14 18:14:11 -0300

    feat: add auto install for pip packages

--- generate/node/dependencies/autoInstallModules.go
+++ generate/node/dependencies/autoInstallModules.go
@@ -41,3 +41,40 @@ func AutoInstallNpm(project string) {
 		}
 	}
 }
+
+func AutoInstallPip(project string) {
+	_, errorPath := os.Stat("./" + project)
+
+	if os.IsNotExist(errorPath) {
+
+	} else {
+
+		f, err := os.Create("./installMod.bat")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer f.Close()
+
+		firstLine := "@ECHO OFF\n\ncd " + project
+		data := []byte(firstLine)
+
+		_, err2 := f.Write(data)
+
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+
+		line2 := "\npy -3 -m venv venv-" + project + "\nvenv-" + project + " \\scripts\\activate\npip install -r requirements.txt"
+		data2 := []byte(line2)
+
+		var idx int64 = int64(len(data))
+
+		_, err3 := f.WriteAt(data2, idx)
+
+		if err3 != nil {
+			log.Fatal(err3)
+		}
+	}
+}
